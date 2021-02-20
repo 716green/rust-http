39d0ed2359f864b74ecf88f8981cef0adc91f776
Bob Bass <bob@bbass.co>
2021-02-20 17:58:51 -0500

    cleaned whitespace

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,3 @@ fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
-
-// let get = Method::GET;
-// let delete = Method::DELETE;
-// let post = Method::POST;
-// let put = Method::PUT;
-// let head = Method::HEAD;
-// let connect = Method::CONNECT;
-// let options = Method::OPTIONS;
-// let trace = Method::TRACE;
-// let patch = Method::PATCH;
