392ed625268e78e1107647a7521a238d8877c931
Bob Bass <bob@bbass.co>
2021-02-19 21:45:43 -0500

    added http methods

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # rust-http
 
 ## Rust HTTP Server
+
+### 29. The Options Enum
+
+[Lesson](https://www.udemy.com/course/rust-fundamentals/learn/lecture/20695668#overview)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
 fn main() {
-    println!("Hello, world!");
+    let get = Method::GET;
+    let delete = Method::DELETE;
+    let post = Method::POST;
+    let put = Method::PUT;
+    let head = Method::HEAD;
+    let connect = Method::CONNECT;
+    let options = Method::OPTIONS;
+    let trace = Method::TRACE;
+    let patch = Method::PATCH;
+
+
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
 }
+
+struct Server {
+    addr: String,
+}
+
+impl Server {
+    // Constructor
+    fn new(addr: String) -> Self {
+        Self { addr }
+    }
+
+    // Methods take first param referencing itself
+    fn run(self) {
+        println!("Listening on {}", self.addr);
+    }
+}
+
+struct Request {
+    path: String,
+    query_string: String,
+    method: Method,
+}
+
+enum Method {
+    GET,
+    DELETE,
+    POST,
+    PUT,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
+}
+
+
+
+/*
+GET /user?id=10 HTTP/1.1\r\n
+HEADERS \r\n
+BODY
+*/
